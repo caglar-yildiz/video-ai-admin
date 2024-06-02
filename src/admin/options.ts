adbc0e4474496f29cbae48eef42daace9d9a232f
cayildiz <cayildiz@innova.com.tr>
2024-06-02 03:57:23 +0300

    first commit

---
+++ src/admin/options.ts
@@ -0,0 +1,65 @@
+import { AdminJSOptions } from 'adminjs';
+import { getModelByName } from '@adminjs/prisma';
+
+import { prisma } from '../db/index.js';
+
+import componentLoader from './component-loader.js';
+
+const options: AdminJSOptions = {
+  componentLoader,
+  rootPath: '/',
+  resources: [
+    {
+      resource: { model: getModelByName('User'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('Organization'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('Subscription'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('Country'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('NewsletterSubscriber'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('Dubbing'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('Language'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('Product'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model: getModelByName('PaymentTransaction'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model : getModelByName('ProductDescription'), client: prisma },
+      options: {},
+    },
+    {
+      resource: { model : getModelByName('FAQ'), client: prisma },
+      options: {},
+    }
+  ],
+  databases: [],
+  branding: {
+    companyName: 'Video AI',
+    withMadeWithLove: false,
+    logo: 'public/branding.png',
+  },
+};
+
+export default options;
